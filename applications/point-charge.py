d0c443abae32b059e484cb8171b8aed5be598b62
Liu Chang <luroylau@163.com>
2020-05-08 15:46:06 +0800

    apply gauss law to point charge

--- applications/point-charge.py
+++ applications/point-charge.py
@@ -1,7 +1,15 @@
 import numpy as np
 from typing import Union
 
+T = Union[int, float]
+
 
 def surfaceIntSphere(r: float) -> float:
     """ Surface integral of 1 over a sphere with radius r """
-    return 4.0 * np.pi * r * r`
+    return 4.0 * np.pi * r * r
+
+
+def pointChargeElecFluxDensity(q: T, r: T) -> float:
+    """ Electric flux density due to point charge. 
+    The direction depends on charge 's sign, and along radial """
+    return q / surfaceIntSphere(r)
